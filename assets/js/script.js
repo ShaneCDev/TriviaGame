e8991cdd5922e6cd5af9577edb9d0fd75683f922
Shane <shaneconway1998@gmail.com>
2022-08-28 21:54:34 +0000

    added api links

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@ const modal = document.getElementById('myModal');
 const openBtn = document.getElementById('openModal');
 const closeBtn = document.getElementById('closeModal');
 
+//API
+const mediumMovieQuiz = "https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple";
+const mediumTVQuiz = "https://opentdb.com/api.php?amount=10&category=14&difficulty=medium&type=multiple";
+const mediumGamesQuiz = "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple";
+const mediumSportsQuiz = "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple";
+
 openBtn.addEventListener('click', () => {
     modal.classList.add('show');
 });
